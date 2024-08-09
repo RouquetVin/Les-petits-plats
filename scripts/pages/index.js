aa8b4b23c525977f9d16c66873678227953042e2
Vincent Rouquet <rouquet.v9081@gmail.com>
2024-08-09 09:32:33 +0200

    Ajout de la recherche simple

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -7,20 +7,76 @@ class MenuApp {
 		this.recipes = recipes;
 		this.menusSection = document.querySelector('.menus_section');
 		this.menusTemplate = new CardTemplate();
+		this.searchInput = document.querySelector('#search');
+		this.searchButton = document.querySelector(
+			'.icone_search_background',
+		);
 	}
 
 	// Method to display all recipes
-	displayAllRecipes() {
-		for (let i = 0; i < this.recipes.length; i++) {
+	displayAllRecipes(recipesToDisplay = this.recipes) {
+		this.menusSection.innerHTML = '';
+		for (let i = 0; i < recipesToDisplay.length; i++) {
 			this.menusSection.appendChild(
-				this.menusTemplate.getMenuCardDom(this.recipes[i]),
+				this.menusTemplate.getMenuCardDom(
+					recipesToDisplay[i],
+				),
 			);
 		}
+	}
+
+	// Method to filter recipes based on user input
+	filterRecipes(inputUser) {
+		inputUser = inputUser.toLowerCase();
+		let tableau = [];
+		if (inputUser.length >= 3) {
+			for (let i = 0; i < this.recipes.length; i++) {
+				let found = false;
+				if (
+					this.recipes[i].name
+						.toLowerCase()
+						.includes(inputUser) ||
+					this.recipes[i].description
+						.toLowerCase()
+						.includes(inputUser)
+				) {
+					tableau.push(this.recipes[i]);
+					found = true;
+				}
+				if (!found) {
+					for (
+						let j = 0;
+						j < this.recipes[i].ingredients.length;
+						j++
+					) {
+						if (
+							this.recipes[i].ingredients[j].ingredient
+								.toLowerCase()
+								.includes(inputUser)
+						) {
+							tableau.push(this.recipes[i]);
+							break;
+						}
+					}
+				}
+			}
+		}
+		return tableau;
+	}
+
+	// Method to handle the search
+	handleSearch() {
+		const query = this.searchInput.value;
+		const filteredRecipes = this.filterRecipes(query);
+		this.displayAllRecipes(filteredRecipes);
 	}
 
 	// Initialization method to start the application
 	init() {
 		this.displayAllRecipes();
+		this.searchButton.addEventListener('click', () =>
+			this.handleSearch(),
+		);
 	}
 }
 
