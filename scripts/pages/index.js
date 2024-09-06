33ec2e07d7878f4b7a785bacd480744a0116df76
Vincent Rouquet <rouquet.v9081@gmail.com>
2024-09-06 10:45:27 +0200

    Modification de la fermeture des tags(tout le tag --> croix)

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -283,7 +283,7 @@ class MenuApp {
 			const removeIcon = document.createElement('i');
 			removeIcon.classList.add('fa', 'fa-times', 'remove-icon');
 
-			tagElement.addEventListener('click', () => {
+			removeIcon.addEventListener('click', () => {
 				this.removeTag(tag, category);
 			});
 
